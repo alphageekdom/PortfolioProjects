7e65968c9710d81798aedf798d21c244d61d2089
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-10-20 09:42:21 -0700

    Update
    Fixed the function so it works without giving any console errors in Chrome.

--- palindromeApp/js/app.js
+++ palindromeApp/js/app.js
@@ -1,10 +1,34 @@
-const text = document.querySelector('#text-input');
-const checkButton = document.querySelector('#button');
+const button = document.querySelector('#button');
 
-
-
-
-
-checkButton.addEventListener('click', () => {
-    console.log('SUCCESS')
+// button does work when clicked
+button.addEventListener('click', () => {
+    console.log("it works")
 })
+
+
+
+function palindrome(str) {
+
+    // Remove unwanted chars and return alphanumeric str
+    let newStr = str
+        .toLowerCase()
+        .match(/[a-z0-9]/gi)
+        .join("");
+
+    // Reverse the newStr
+    let revStr = newStr
+        .split("")
+        .reverse()
+        .join("");
+
+    // Check if string is a palindrome
+    if (newStr === revStr) {
+        console.log("Yes, it's a palindrome.")
+        return true;
+    } else {
+        console.log("Not, it's not a palindrome.")
+        return false;
+    }
+}
+
+palindrome("A man, a plan, a canal. Panama");
